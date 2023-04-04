99fab0248d94cdb3118d7e0ee26b6c9ee3a21a0f
Ankit Gupta <ankitgupta38017@gmail.com>
2023-04-05 01:02:57 +0530

    Added navigation

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Layout } from "antd";
 import HeaderContent from './Header';
+import Navigation from './Navigation'
 
 function App() {
     return (
         <Layout style={{ minHeight: '100vh' }}>
+            <Navigation />
             <Layout style={{ minHeight: '100vh' }}>
                 <HeaderContent />
             </Layout>
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,11 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './app';
-
+import { AppContextProvider } from './context/appContext';
 
 ReactDOM.render(
-    <App />,
+    <AppContextProvider>
+        <App />
+    </AppContextProvider>,
     document.getElementById('root')   
 )
