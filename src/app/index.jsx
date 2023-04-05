5c849264dcd1d6398ce63b16bb51b7300fa62be8
Ankit Gupta <ankitgupta38017@gmail.com>
2023-04-06 00:16:58 +0530

    Completed Header and Navigation

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,17 +1,31 @@
-import React from "react";
-import { Layout } from "antd";
-import HeaderContent from './Header';
-import Navigation from './Navigation'
+import React, { useEffect } from 'react';
+
+import Router from '@/router';
+
+import { Layout, notification } from 'antd';
+
+import Navigation from '@/app/Navigation';
+
+import { useSelector } from 'react-redux';
+import { selectAuth } from '@/redux/auth/selectors';
+import HeaderContent from '@/app/HeaderContent';
+import { useNetworkState } from 'react-use';
 
 function App() {
+  const { isLoggedIn } = useSelector(selectAuth);
+
+  if (!isLoggedIn) return <Router />;
+  else {
     return (
+      <Layout style={{ minHeight: '100vh' }}>
+        <Navigation />
         <Layout style={{ minHeight: '100vh' }}>
-            <Navigation />
-            <Layout style={{ minHeight: '100vh' }}>
-                <HeaderContent />
-            </Layout>
+          <HeaderContent />
+          <Router isLoggedIn={true} />
         </Layout>
+      </Layout>
     );
+  }
 }
 
 export default App;
