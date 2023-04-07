fcacfacc01ccfa53ccbe895d34fb139ddf497cab
Ankit Gupta <ankitgupta38017@gmail.com>
2023-04-07 23:12:38 +0530

    Added hooks

---
+++ src/hooks/useNetwork/index.test.jsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import userEvent from '@testing-library/user-event';
+import useNetwork from './index';
+import { renderHook } from '@testing-library/react-hooks';
+
+const mockDispatch = jest.fn();
+jest.mock('react-redux', () => ({
+  useSelector: jest.fn(),
+  useDispatch: () => mockDispatch,
+}));
+test('Test useNetwork', () => {
+  const { result } = renderHook(() => useNetwork());
+  window.ononline = () => {
+    expect(result.current.isOnline).toBe(true);
+  };
+  window.onoffline = () => {
+    expect(result.current.isOnline).toBe(false);
+  };
+});
