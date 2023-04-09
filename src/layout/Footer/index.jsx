dc61208eaa55c57738287beb75b242c739bd07b1
Ankit Gupta <ankitgupta38017@gmail.com>
2023-04-10 00:06:54 +0530

    Added footer and index layout

---
+++ src/layout/Footer/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Layout } from 'antd';
+
+const { Footer } = Layout;
+
+const FooterContent = () => (
+  <Footer style={{ textAlign: 'center' }}>Ant Design ©2023 Created by US</Footer>
+);
+
+export default FooterContent;
